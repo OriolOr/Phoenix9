6e75e2eb01756248bb5be9164c270203d0018c76
OriolOr <40197427+OriolOr@users.noreply.github.com>
2022-06-08 01:01:17 +0200

    remove unnused resource file (service project)

--- backend/source/Services/IUserCredentialsService.cs
+++ backend/source/Services/IUserCredentialsService.cs
@@ -5,7 +5,5 @@ namespace OriolOr.Maneko.Services
     internal interface IUserCredentialsService
     {
         public bool CheckCredentials(UserCredentials userCredentials);
-
-        public string EncodeMD5HashPassword(string password);
     }
 }
