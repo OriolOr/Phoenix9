73a8a31007bffc89894e153243ea9d3de38fccf0
OriolOr <40197427+OriolOr@users.noreply.github.com>
2022-06-27 19:32:39 +0200

    update AccountDataRepsoitory (fix bug🐛)

--- backend/source/Infrastructure/AccountDataRepository.cs
+++ backend/source/Infrastructure/AccountDataRepository.cs
@@ -25,12 +25,13 @@ namespace OriolOr.Maneko.Infrastructure
             var accDocumentLastUpdate = new DateTime();
 
             if (nDocuments != 0) accDocumentLastUpdate = collection.Find(FilterDefinition<Account>.Empty).FirstOrDefault().LastUpdate;
-
+             
             if (accDocumentLastUpdate.Day != DateTime.Now.Day)
             {
 
                 WebScrapper webscrapper = new WebScrapper(userCredentials);
 
+                //CREATE ACCOUNT OBJECT 
                 Account account = new Account()
                 {
                     CurrentBalance = webscrapper.ScrapCurrentBalance(),
@@ -38,15 +39,24 @@ namespace OriolOr.Maneko.Infrastructure
                     LastUpdate = DateTime.Now
                 };
 
+                //CREATE OBJECT TO BE INSERED IN DATABASE 
                 var newYearBalance = new YearBalance();
                 newYearBalance.Year = 2022;
                 newYearBalance.MonthBalances = JsonConvert.DeserializeObject<Collection<MonthBalance>>(Resources.BankData);
 
                 account.YearHistory.Add(newYearBalance);
 
-                collection.InsertOne(account);
+                //iNSERT TO DATABASE
+                if (this.CheckAccountIdExists(database, account.AccountNumberId))
+                {
+                    //Update existing element in database 
+                    collection.ReplaceOne(ac => ac.AccountNumberId == account.AccountNumberId, account);
+                }
+                else
+                {
+                    collection.InsertOne(account);
+                }
             }
-
         }
 
         public double GetAccountCurrentBalance(IMongoDatabase database)
@@ -63,6 +73,23 @@ namespace OriolOr.Maneko.Infrastructure
 
             var accDocument = collection.Find(FilterDefinition<Account>.Empty).FirstOrDefault();
             return accDocument.YearHistory;
+        }
+
+        private bool CheckAccountIdExists(IMongoDatabase database, string accountId)
+        {
+            var collection = database.GetCollection<Account>(this.CollectionName);
+
+            var accElements = collection.Find(acc => acc.AccountNumberId == accountId).Count();
+
+            if (accElements == 0)
+            {
+                return false; 
+            }
+            else
+            {
+                return true;
+            }
+
         }
     }
 
