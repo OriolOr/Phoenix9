902e028359873cc75d5e349256f08e356de3d8a6
Oriol <oriol.orus.or@gmail.com>
2023-04-15 00:37:11 +0200

    front router in progress

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import {BrowserRouter, Link} from "react-router-dom"
 import Footer  from "./components/footer/footer";
 import Header from "./components/header/header";
 import Login  from "./components/login/login";
+
 
 export const App:React.FC = () => {
   
   return(
     <div className = "appContainer">
-    <Header/>
-    <Login/>
-    <Footer/>
+      <Header/>
+      <BrowserRouter>
+        <Link>
+          <Login/>
+        </Link>
+      </BrowserRouter>
+      <Footer/>
     </div>
 )
 };
