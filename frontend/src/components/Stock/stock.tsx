79ee758ae9bf81ea1bf230c0db079d5a051a4cf2
Oriol Orús Ordonez <orusordo@emea.roche.com>
2024-01-31 19:57:35 +0100

    update stock component

--- frontend/src/components/Stock/stock.tsx
+++ frontend/src/components/Stock/stock.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Stock: React.FC = () => {
+
+   const [stockValue, setStockValue] = useState(0);
+
     return (
         <div>
            Stock
+           Current stock price: {stockValue}
         </div>
     );
+
+
+    function getStockValue() {}
 };
 
 export default Stock;
