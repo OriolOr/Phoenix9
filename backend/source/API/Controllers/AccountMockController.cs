b7f5390d351a6d62c7b361fa204bc9471e28e67b
Oriol Orús Ordonez <orusordo@emea.roche.com>
2024-05-07 15:38:14 +0200

    update MonthBalance domain

--- backend/source/API/Controllers/AccountMockController.cs
+++ backend/source/API/Controllers/AccountMockController.cs
@@ -23,12 +23,12 @@ namespace OriolOr.Maneko.API.Controllers
 
             //save data in file or database.
             var balance = new Collection<MonthBalance>();
-            balance.Add(new MonthBalance(){Month = "January", InitialBalance = 200, FinalBalance = 90});
-            balance.Add(new MonthBalance() {Month = "February", InitialBalance = 1298, FinalBalance = 545});
-            balance.Add(new MonthBalance() {Month = "March", InitialBalance = 4959, FinalBalance = 8049});
-            balance.Add(new MonthBalance() { Month = "April", InitialBalance = 12034, FinalBalance = 11590 });
-            balance.Add(new MonthBalance() { Month = "May", InitialBalance = 12998, FinalBalance = 15455 });
-            balance.Add(new MonthBalance() { Month = "June", InitialBalance = 14959, FinalBalance = 13049 });
+            balance.Add(new MonthBalance(){Month = MonthEnum.January, InitialBalance = 200, FinalBalance = 90});
+            balance.Add(new MonthBalance() {Month = MonthEnum.February, InitialBalance = 1298, FinalBalance = 545});
+            balance.Add(new MonthBalance() {Month = MonthEnum.March, InitialBalance = 4959, FinalBalance = 8049});
+            balance.Add(new MonthBalance() { Month = MonthEnum.April, InitialBalance = 12034, FinalBalance = 11590 });
+            balance.Add(new MonthBalance() { Month = MonthEnum.May, InitialBalance = 12998, FinalBalance = 15455 });
+            balance.Add(new MonthBalance() { Month = MonthEnum.June, InitialBalance = 14959, FinalBalance = 13049 });
 
             var yearBalance = new YearBalance()
             {
