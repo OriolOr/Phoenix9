1c8733159fcb6e6e74a4fc46aa65c1e24a0e288d
Oriol Orús <oriol.orus.or@gmail.com>
2026-04-22 15:54:19 +0200

    update to .net 8.0

---
+++ backend/source/API/Service/Interfaces/IMortageService.cs
@@ -0,0 +1,6 @@
+﻿namespace OriolOr.Maneko.API.Service.Interfaces;
+
+public class IMortageService
+{
+    
+}
---
+++ backend/source/API/Service/MortageService.cs
@@ -0,0 +1,6 @@
+﻿namespace OriolOr.Maneko.API.Service;
+
+public class MortageService
+{
+    
+}
